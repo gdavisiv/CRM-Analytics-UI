1ef2022af0a3fc331a843d6939eb19abfa5965e9
George Davis IV <gdavisiv@gmail.com>
2020-09-21 22:49:57 -0400

    UIUpdates

--- CRM-Analytics-UI/ContentView.swift
+++ CRM-Analytics-UI/ContentView.swift
@@ -9,12 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Image(systemName: "line.horizontal.3")
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .background(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/))
     }
 }
