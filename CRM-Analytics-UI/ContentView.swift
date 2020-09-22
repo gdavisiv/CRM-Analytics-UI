ed47062a2b891e34b15185e2ae20b909d1b40fa1
George Davis IV <gdavisiv@gmail.com>
2020-09-21 22:26:01 -0400

    Initial Commit

---
+++ CRM-Analytics-UI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CRM-Analytics-UI
+//
+//  Created by George Davis IV on 9/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
