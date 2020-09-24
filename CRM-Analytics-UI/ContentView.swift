a5284acd5b5e3663b4af39bfdedd6e7cf0ae92dd
George Davis IV <gdavisiv@gmail.com>
2020-09-23 22:47:22 -0400

    UIUpdate: Updated Background

--- CRM-Analytics-UI/ContentView.swift
+++ CRM-Analytics-UI/ContentView.swift
@@ -30,6 +30,14 @@ struct Home: View {
                         .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                         .shadow(radius: 1)
                     
+                    Spacer()
+                    
+                    Button(action: {}) {
+                        Image(systemName: "bell")
+                            .renderingMode(.template)
+                            .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                    }
+                    
                 }
 
             }
